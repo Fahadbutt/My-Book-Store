ca617e751eb1757ccc649e3465f1269d0acdfb66
Fahad Butt <fahadbutt.qc@gmail.com>
2015-08-10 12:32:32 +0500

    updated

---
+++ Book.js
@@ -0,0 +1,14 @@
+/**
+ * Created by fahad on 8/6/15.
+ */
+function book_create(Book_name,Book_Edition,Book_Author,Book_Price,Books_Available)
+{
+    this.Book_name=Book_name;
+    this.Book_Edition=Book_Edition;
+    this.Book_Author=Book_Author;
+    this.Book_Price=Book_Price;
+    this.Books_Available=Books_Available;
+}
+
+
+
